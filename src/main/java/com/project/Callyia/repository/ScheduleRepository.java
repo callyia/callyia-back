91baecbf6b0687e79bc1024d8114007e93198d27
임성훈 <dlatjdgns999@gmail.com>
2024-01-23 10:32:39 +0900

    memberService

--- src/main/java/com/project/Callyia/repository/ScheduleRepository.java
+++ src/main/java/com/project/Callyia/repository/ScheduleRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s, w " +
             " FROM Schedule s LEFT JOIN s.member w " +
             " WHERE s.sno = :sno")
     Object getScheduleBySno(@Param("sno") Long sno);
+
+    @Query("SELECT s, m.nickname " +
+            "FROM Schedule s " +
+            "JOIN s.member m")
+    List<Object[]> getNickname();
 }
---
+++ src/test/java/com/project/Callyia/repository/ScheduleRepositoryTests.java
@@ -0,0 +1,26 @@
+package com.project.Callyia.repository;
+
+import com.project.Callyia.dto.ScheduleDTO;
+import com.project.Callyia.service.ScheduleService;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Log4j2
+class ScheduleRepositoryTests {
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    List<ScheduleDTO> result = scheduleService.getAllSchedule();
+    @Test
+    void getNickname() {
+        log.info(result);
+    }
+}
